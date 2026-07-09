3087f8d0650204874da642395065b81d0e651466
Dipankar Dutta <saitankoresaitani@gmail.com>
2026-07-09 19:30:27 +0530

    Fixed for vercel not to look into the node modules

--- ImpactForge_FormFix/next.config.mjs
+++ ImpactForge_FormFix/next.config.mjs
@@ -2,12 +2,12 @@
 const nextConfig = {
   serverExternalPackages: ["tesseract.js"],
   
-  outputFileTracingIncludes: {
-    // The wildcard tells Vercel to attach these modules to every backend route within the workspace root
-    '/**/*': [
-      'node_modules/tesseract.js-core/tesseract-core-simd.wasm',
-      'node_modules/tesseract.js/src/worker-script/node/index.js'
-    ],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // This completely shields Vercel from trying to look inside the tesseract folder
+      config.externals = [...(config.externals || []), 'tesseract.js'];
+    }
+    return config;
   },
 };
 
