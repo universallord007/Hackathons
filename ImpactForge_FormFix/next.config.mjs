82c252be769ce65a2df2f164f367e07eb36bc7b9
Dipankar Dutta <saitankoresaitani@gmail.com>
2026-07-09 18:50:06 +0530

    The next.config.mjs is changed for tesserect issues

--- ImpactForge_FormFix/next.config.mjs
+++ ImpactForge_FormFix/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["tesseract.js"],
+  outputFileTracingIncludes: {
+    '/api/**/*': [
+      './node_modules/tesseract.js-core/tesseract-core-simd.wasm',
+      './node_modules/tesseract.js/src/worker-script/node/index.js'
+    ],
+  },
 };
 
 export default nextConfig;
