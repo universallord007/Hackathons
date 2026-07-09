03459b3ce3786478665f63e779ab06c2baaa7e9e
Dipankar Dutta <saitankoresaitani@gmail.com>
2026-07-09 19:04:29 +0530

    Again fixed the directories

--- ImpactForge_FormFix/next.config.mjs
+++ ImpactForge_FormFix/next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["tesseract.js"],
+  
   outputFileTracingIncludes: {
-    '/api/**/*': [
-      './node_modules/tesseract.js-core/tesseract-core-simd.wasm',
-      './node_modules/tesseract.js/src/worker-script/node/index.js'
+    // The wildcard tells Vercel to attach these modules to every backend route within the workspace root
+    '/**/*': [
+      'node_modules/tesseract.js-core/tesseract-core-simd.wasm',
+      'node_modules/tesseract.js/src/worker-script/node/index.js'
     ],
   },
 };
